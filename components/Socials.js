090e1982d21a84c010c3d284d958222dd785a8f9
AlfredoSantos20 <AlfredoSantos20>
2024-06-10 11:24:18 +0800

    updated

--- components/Socials.js
+++ components/Socials.js
@@ -16,10 +16,10 @@ const Socials = () => {
   return (
       <div className='flex items-center gap-x-5 text-lg'>
     
-          <Link href={''} className='hover:text-accent transition-all duration-300'>
+          <Link href={'https://www.facebook.com/tazki.santosjr'} className='hover:text-accent transition-all duration-300'>
             <RiFacebookLine/>
           </Link>
-          <Link href={''} className='hover:text-accent transition-all duration-300'>
+          <Link href={'https://www.instagram.com/afrdo_sntsjr/?hl=en'} className='hover:text-accent transition-all duration-300'>
             <RiInstagramLine/>
           </Link>
           <Link href={''} className='hover:text-accent transition-all duration-300'>
