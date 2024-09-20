8a29b1cb7fae04c2f6579d005c2c2b03a7127a24
AlfredoSantos20 <AlfredoSantos20>
2024-09-20 17:00:46 +0800

    some updates

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -11,7 +11,7 @@ import { BsArrowDown } from 'react-icons/bs';
 const ProjectsBtn = () => {
   return (
   <div className='mx-auto xl:mx-0'>
-    <Link  title="Download Resume" target='_blank' href="/my-resume.pdf" download className='relative w-[185px] h-[185px] flex justify-center items-center
+    <Link  title="Download Resume" target='_blank' href="/Alfredo B Santos Jr - resume.pdf" download className='relative w-[185px] h-[185px] flex justify-center items-center
       bg-circleStar bg-cover bg-center bg-no-repeat group z-10'>
       <Image src={'/myrounded-3.png'} width={141} height={148} alt='' className='animate-spin-slow w-fill h-full max-w-[141px] 
       max-h-[148px]'/>
